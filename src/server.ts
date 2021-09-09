5dc10a6164ad7f3cd0dacad50be97dd82476baa4
zackorykelly <zackkelly@live.ca>
2021-09-09 01:51:09 -0400

    Add report controller

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import App from './app';
 import PostsController from './posts/posts.controller';
 import AuthenticationController from './authentication/authentication.controller';
 import UserController from './users/user.controller';
+import ReportController from './report/report.controller';
 
 import validateEnv from './utils/validateEnv';
 
@@ -12,7 +13,8 @@ const app = new App(
     [
         new PostsController(),
         new AuthenticationController(),
-        new UserController()
+        new UserController(),
+        new ReportController()
     ]
 );
 
